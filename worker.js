359cee9f9a03c2e843a16212a6ff991f20f10bd5
Deswilliams <desw@hotmail.com>
2026-09-18 19:49:35 +1000

    Fix scheduled dateTime reference to round schedule

--- worker.js
+++ worker.js
@@ -258,7 +258,7 @@ for (const round of data.rounds || []) {
       round: round.name,
       gameId: game.id,
       status: game.status,
-      scheduled: game.schedule?.[0]?.dateTime || null,
+scheduled: round.schedule?.[0]?.dateTime || null,
       teams: (game.teams || []).map(team => ({
         name: team.name,
         home: team.isHomeTeam,
